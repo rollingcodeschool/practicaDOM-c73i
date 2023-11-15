f0cee87064e885c9c3d774a08ec811f198f490b4
Emilse Arias <emilsearias@gmail.com>
2023-11-15 11:42:59 -0300

    agregamos una funcion para cambiar el titulo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('estoy viendo el archivo de js')
+// const botonCambiarTitulo = document.getElementsByTagName('button');
+// const botonCambiarTitulo = document.getElementsByClassName('btn');
+// const botonCambiarTitulo = document.getElementById('btnCambiarTitulo');
+const botonCambiarTitulo = document.querySelector('#btnCambiarTitulo');
+console.log(botonCambiarTitulo);
+
+function cambiarTitulo(){
+    console.log('estoy en cambiar el titulo');
+    //selecciono el titulo
+    const tituloPrincipal = document.querySelector('h1');
+    //modifico ese titulo
+    tituloPrincipal.innerHTML = 'Titulo <b> nuevo </b>';
+}
+
+
+//agregamos un evento el cúal llama a una funcion
+
+botonCambiarTitulo.addEventListener('click', cambiarTitulo )
