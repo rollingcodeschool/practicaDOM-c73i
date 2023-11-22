876168c9d674e5dcb814ac4f84b6ece6ea52eaea
Emilse Arias <emilsearias@gmail.com>
2023-11-22 10:26:09 -0300

    agregamos un nuevo parrafo en la seccion de datos antes del botón ver mas

--- js/app.js
+++ js/app.js
@@ -15,6 +15,25 @@ function cambiarTitulo(){
     tituloPrincipal.className = 'display-1 tituloPrincipal'
 }
 
+const verMas = () =>{
+    console.log('Desde la funcion ver mas');
+    // OPCION 1:
+    //creo un nodo nuevo
+    const parrafoNuevo = document.createElement('p');
+    console.log(parrafoNuevo);
+    // preparar el nodo
+    parrafoNuevo.innerHTML = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos aut cum a numquam ut nisi dolor hic! Explicabo quasi porro numquam ex enim, maiores soluta perferendis, at recusandae veritatis ab!`;
+    parrafoNuevo.className = 'lead';
+    //insertar el nodo en el DOM
+    const seccionDatos = document.querySelector('#seccionDatos');
+    // seccionDatos.appendChild(parrafoNuevo); //agrega un nodo hijo al final
+    // seccionDatos.prepend(parrafoNuevo); //agrega un nodo hijo al principio
+    // seccionDatos.prepend(parrafoNuevo); //agrega un nodo hijo al principio
+    const btnVerMas = document.getElementsByClassName('btn');
+    console.log(btnVerMas[2]);
+    seccionDatos.insertBefore(parrafoNuevo, btnVerMas[2]);
+}
+
 
 //agregamos un evento el cúal llama a una funcion
 
